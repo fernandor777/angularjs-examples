3c815e42c9bcb89d4b738a858cb9145a6eb7bd07
Fernando Miño <fernandomino7@gmail.com>
2016-07-08 12:17:19 -0500

    bootstrap nav

---
+++ bootstrap-nav/js/main.js
@@ -0,0 +1,41 @@
+/* 
+ * Origami solutions
+ */
+
+var contextName = "smb-inmobiliaria";
+
+var app = angular.module("myApp", ['ngRoute', 'ui.bootstrap']);
+
+app.controller("myCtrl", function($scope) {
+    
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+    
+});
+
+app.config(function($routeProvider, $locationProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : '/'+contextName+'/pages/main.html',
+                controller  : 'myCtrl'
+            })
+
+            // route for the about page
+            .when('/about', {
+                templateUrl : 'pages/about.html',
+                controller  : 'aboutController'
+            })
+
+            // route for the contact page
+            .when('/contact', {
+                templateUrl : 'pages/contact.html',
+                controller  : 'contactController'
+            });
+        // use the HTML5 History API
+        //$locationProvider.html5Mode(true);
+    });
+
+
+
